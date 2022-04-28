b0962973da8a115a9252697298b7d072fb1334dc
Riads77 <adrienroumegous@hotmail.com>
2022-04-28 11:11:02 +0200

    Extract all datas from Dracor fre

--- Conversion and scraping/downloadDracor.py
+++ Conversion and scraping/downloadDracor.py
@@ -1,30 +1,7 @@
 import glob, os, re, sys, time, requests, json
 
-# response = requests.get("https://dracor.org/api/corpora", 'metrics')
-# print(response.status_code)
-# print(response.json())
+data_dic = json.loads(requests.get("https://dracor.org/api/corpora/fre", 'metrics').content)
 
-# def jprint(obj):
-#     # create a formatted string of the Python JSON object
-#     text = json.dumps(obj, sort_keys = True, indent = 4)
-#     print(text)
+plays = data_dic.get('dramas')
 
-# jprint(response.json())
-
-
-response_fre = requests.get("https://dracor.org/api/corpora/fre", 'metrics')
-
-def j_to_string(obj):
-    # create a formatted string of the Python JSON object
-    return json.dumps(obj, sort_keys = True, indent = 4)
-
-print(j_to_string(response_fre.json()))
-
-# # Get the current folder
-# folder = os.path.abspath(os.path.dirname(sys.argv[0])) 
-
-# # List of plays to generate
-
-# # TODO : Créer classe qui stocke option d'ajouts de fichiers.
-
-# # https://dracor.org/api/corpora/fre/play/
+print(plays[0].keys())
