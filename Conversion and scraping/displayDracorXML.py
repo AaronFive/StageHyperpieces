9ccaa02eb3ca112ed0d68e497c0f5d8cd5775c1a
Riads77 <adrienroumegous@hotmail.com>
2022-06-10 16:58:42 +0200

    Generate dots and factorise tree

--- Conversion and scraping/displayDracorXML.py
+++ Conversion and scraping/displayDracorXML.py
@@ -1,6 +1,7 @@
 from xml.dom import minidom
 from os.path import abspath, dirname, join, exists
 import os, sys
+from os import walk
 
 folder = abspath(dirname(sys.argv[0]))
 root = join(folder, os.pardir)
@@ -10,41 +11,8 @@ outputs_folder = abspath(join(root, "DracorTrees"))
 if not exists(outputs_folder):
     os.makedirs(outputs_folder)
 
-def writeStart(output, node):
-    # output.write("graph Tree {node [shape=record,height=.1]\n")
-    # output.write("edge [tailclip=false, arrowtail=dot, dir=both];\n")
-    output.write("graph Tree {\n")
-
-# def export_dot(node):
-#     """Renvoie une chaîne encodant le graphe au format dot."""
-#     liste_sommets = node.sommets()
-#     liste_aretes = node.aretes()
-
-#     chaine = "graph G {\n"
-
-#     for sommet in liste_sommets:
-#         chaine += "\t{0} [label = \"{1}\"];\n".format(sommet, node.nom_sommet(sommet))
-
-#     for u, v, station in liste_aretes:
-#         chaine += "\t{0} -> {1} [label = \"{2}\"];\n".format(u, v, station)
-
-#     chaine += "}"
-
-#     return chaine
-
-# def writeNodes(output, node, n=0):
-#     output.write("\t\"t{0}\" [label = \"{1}\"];\n".format(n, node.nodeName))
-#     for child in node.childNodes:
-#         n = writeNodes(output, child, n + 1)
-#     return n
-
-# def writeLinks(output, node, n=0):
-#     m = n
-#     for child in node.childNodes:
-#         m += 1
-#         output.write("\t\"t{0}\" -- \"t{1}\";\n".format(n, m))
-#         m = writeLinks(output, child, m)
-#     return m
+def writeStart(output):
+    output.write("digraph Tree {\n")
 
 def writeNodes(output, node, n=0):
     if not '#' in node.nodeName:
@@ -59,18 +27,18 @@ def writeLinks(output, node, n=0):
     for child in node.childNodes:
         if not '#' in child.nodeName:
             m += 1
-            output.write("\t\"t{0}\" -- \"t{1}\";\n".format(n, m))
+            output.write("\t\"t{0}\" -> \"t{1}\";\n".format(n, m))
             m = writeLinks(output, child, m)
     return m
 
-def writeEnd(output, node):
+def writeEnd(output):
     output.write("}\n")
 
 def parse_dot(output, node):
-    writeStart(output, node)
+    writeStart(output)
     writeNodes(output, node)
     writeLinks(output, node)
-    writeEnd(output, node)
+    writeEnd(output)
 
 def generate_graph(path):
     from os import walk
@@ -89,23 +57,82 @@ def parse_xml(output, node, indent=-1):
         parse_xml(output, child, indent + 1)
 
 def parse_plays(path):
-    from os import walk
-    files = list(map(lambda f: join(path, f), next(walk(path), (None, None, []))[2]))
-    for file in files:
+    for file in list(map(lambda f: join(path, f), next(walk(path), (None, None, []))[2])):
         name = file.split('/')[-1].replace('xml', 'txt')
         print(f"Converting {file}")
         with open(join(outputs_folder, name), 'w') as f:
             parse_xml(f, minidom.parse(file))
 
 def export_svg(path):
-    from os import walk
-    files = list(filter(lambda f: '.dot' in f, map(lambda f: join(path, f), next(walk(path), (None, None, []))[2])))
-    for file in files:
+    for file in list(filter(lambda f: '.dot' in f, map(lambda f: join(path, f), next(walk(path), (None, None, []))[2]))):
         print(f"Exporting {file}")
         os.system(f"dot -Tsvg {file} -o {file.replace('.dot', '.svg')}")
         break
 
+def parse_same_nodes(node):
+    name = node.nodeName
+    res = []
+    if name[0] != '#':
+        res.append(node.nodeName)
+    for child in node.childNodes:
+        res.extend(parse_same_nodes(child))
+    return res
+
+def parse_same_links(node):
+    name = node.nodeName
+    res = []
+    if name[0] != '#':
+        for child in node.childNodes:
+            if child.nodeName[0] != '#':
+                res.append((node.nodeName, child.nodeName))
+                res.extend(parse_same_links(child))
+    return res
+
+def find_same_nodes(path):
+    nodes = []
+    links = []
+    for file in list(map(lambda f: join(path, f), next(walk(path), (None, None, []))[2])):
+        name = file.split('/')[-1].replace('xml', 'txt')
+        print(f"Check nodes of {file}")
+        root = minidom.parse(file).childNodes[0]
+        res = parse_same_nodes(root)
+        if not nodes:
+            nodes = list(set(res.copy()))
+        else:
+            nodes = list(filter(lambda node: node in res, nodes))
+        res2 = parse_same_links(root)
+        if not links:
+            links = list(set(res2.copy()))
+        else:
+            links = list(filter(lambda link: link in res2, links))
+    return nodes, links
+
+def create_common_tree(nodes, links):
+    with open(join(folder, 'common_tree.dot'), 'w') as output:
+        writeStart(output)
+        for i in range(len(nodes)):
+            output.write("\t\"t{0}\" [label = \"{1}\"];\n".format(i, nodes[i]))
+        for father, son in links:
+            output.write("\t\"t{0}\" -> \"t{1}\";\n".format(nodes.index(father), nodes.index(son)))
+        writeEnd(output)
+
+# def parse_paths(node, path_buffer=''):
+#     name = node.nodeName
+#     if name[0] != '#':
+#         print(name)
+#     for child in node.childNodes:
+#         parse_paths(child, '/'.join([path_buffer, name]))   
+
+# def parse_plays(path):
+#     for file in list(map(lambda f: join(path, f), next(walk(path), (None, None, []))[2])):
+#         name = file.split('/')[-1].replace('xml', 'txt')
+#         print(f"Converting {file}")
+#         parse_paths(f, minidom.parse(file))         
+
 if __name__ == "__main__":
-    parse_plays(dracor_folder)
-    generate_graph(dracor_folder)
+    # parse_plays(dracor_folder)
+    # generate_graph(dracor_folder)
     # export_svg(outputs_folder)
+    nodes, links = find_same_nodes(dracor_folder)
+    create_common_tree(nodes, links)
+
