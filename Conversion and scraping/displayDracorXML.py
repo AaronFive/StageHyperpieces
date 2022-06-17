659af8cedec87f8aa9511dae2db99d70c8724baa
Riads77 <adrienroumegous@hotmail.com>
2022-06-17 16:24:14 +0200

    Args in DracorXML and partial implementation of it

--- Conversion and scraping/displayDracorXML.py
+++ Conversion and scraping/displayDracorXML.py
@@ -1,53 +1,61 @@
 from xml.dom import minidom
 from os.path import abspath, dirname, join, exists
 import os, sys
+import argparse
+
 from os import walk
 
 folder = abspath(dirname(sys.argv[0]))
 root = join(folder, os.pardir)
-dracor_folder = abspath(join(root, "corpusDracor"))
-outputs_folder = abspath(join(root, "DracorTrees"))
+inputs_folder = abspath(join(root, "corpusDracor"))
+outputs_folder = abspath(join(root, "treesDracor"))
+common_trees_folder = abspath(join(root, "commonTrees"))
 
 if not exists(outputs_folder):
     os.makedirs(outputs_folder)
 
+def get_file_name(folder):
+    return folder.split('/')[-1]
+
 def writeStart(output):
     output.write("digraph Tree {\n")
 
-def writeNodes(output, node, n=0):
+def writeNodes(output, node, height, n=0):
     if not '#' in node.nodeName:
         output.write("\t\"t{0}\" [label = \"{1}\"];\n".format(n, node.nodeName))
-        for child in node.childNodes:
-            n = writeNodes(output, child, n + 1)
+        if height != 0:
+            for child in node.childNodes:
+                n = writeNodes(output, child, height - 1, n + 1)
         return n
     return n - 1
 
-def writeLinks(output, node, n=0):
+def writeLinks(output, node, height, n=0):
     m = n
-    for child in node.childNodes:
-        if not '#' in child.nodeName:
-            m += 1
-            output.write("\t\"t{0}\" -> \"t{1}\";\n".format(n, m))
-            m = writeLinks(output, child, m)
+    if height != 0:
+        for child in node.childNodes:
+            if not '#' in child.nodeName:
+                m += 1
+                output.write("\t\"t{0}\" -> \"t{1}\";\n".format(n, m))
+                m = writeLinks(output, child, height - 1, m)
     return m
 
 def writeEnd(output):
     output.write("}\n")
 
-def parse_dot(output, node):
+def parse_dot(output, node, height):
     writeStart(output)
-    writeNodes(output, node)
-    writeLinks(output, node)
+    writeNodes(output, node, height)
+    writeLinks(output, node, height)
     writeEnd(output)
 
-def generate_graph(path):
+def generate_graph(path, height=-1):
     from os import walk
     files = list(map(lambda f: join(path, f), next(walk(path), (None, None, []))[2]))
     for file in files:
-        name = file.split('/')[-1].replace('xml', 'dot')
+        name = get_file_name(file).replace('xml', 'dot')
         print(f"Generate graph : {name}")
         with open(join(outputs_folder, name), 'w') as f:
-            parse_dot(f, minidom.parse(file).childNodes[0])
+            parse_dot(f, minidom.parse(file).childNodes[0], height)
 
 def parse_xml(output, node, indent=-1):
     name = node.nodeName
@@ -63,11 +71,11 @@ def parse_plays(path):
         with open(join(outputs_folder, name), 'w') as f:
             parse_xml(f, minidom.parse(file))
 
-def export_svg(path):
-    for file in list(filter(lambda f: '.dot' in f, map(lambda f: join(path, f), next(walk(path), (None, None, []))[2]))):
-        print(f"Exporting {file}")
-        os.system(f"dot -Tsvg {file} -o {file.replace('.dot', '.svg')}")
-        break
+# def export_svg(path):
+#     for file in list(filter(lambda f: '.dot' in f, map(lambda f: join(path, f), next(walk(path), (None, None, []))[2]))):
+#         print(f"Exporting {file}")
+#         os.system(f"dot -Tsvg {file} -o {file.replace('.dot', '.svg')}")
+#         break
 
 def parse_same_nodes(node, path_buffer=''):
     name = node.nodeName
@@ -94,7 +102,7 @@ def find_same_nodes(path):
     nodes = []
     links = []
     for file in list(map(lambda f: join(path, f), next(walk(path), (None, None, []))[2])):
-        name = file.split('/')[-1].replace('xml', 'txt')
+        name = get_file_name(file).replace('xml', 'txt')
         print(f"Check nodes of {file}")
         root = minidom.parse(file).childNodes[0]
         res = parse_same_nodes(root)
@@ -109,8 +117,8 @@ def find_same_nodes(path):
             links = list(filter(lambda link: link in res2, links))
     return nodes, links
 
-def create_common_tree(nodes, links):
-    with open(join(folder, 'common_tree.dot'), 'w') as output:
+def create_common_tree(nodes, links, file):
+    with open(join(common_trees_folder, file), 'w') as output:
         writeStart(output)
         for node in nodes:
             output.write("\t\"{0}\" [label = \"{1}\"];\n".format(node, node.split('/')[-1]))
@@ -118,27 +126,92 @@ def create_common_tree(nodes, links):
             output.write("\t\"{0}\" -> \"{1}\";\n".format(father, son))
         writeEnd(output)
 
-def parse_paths_from_file(node, path_buffer=''):
-    nodes = []
-    name = node.nodeName
-    if name[0] != '#':
-        name = '/'.join([path_buffer, name])
-        print(name)
-        for child in node.childNodes:
-            parse_paths_from_file(child, name)   
+def clean_outputs_directory(path):
+    os.system('rm ' + path + '/*')
+    print('Delete files from', path)
 
-def parse_paths(path):
-    for file in list(map(lambda f: join(path, f), next(walk(path), (None, None, []))[2])):
-        name = file.split('/')[-1].replace('xml', 'txt')
-        print(f"Look path from {file}")
-        parse_paths_from_file(minidom.parse(file).childNodes[0])  
-        break       
+# def parse_paths_from_file(node, path_buffer=''):
+#     nodes = []
+#     name = node.nodeName
+#     if name[0] != '#':
+#         name = '/'.join([path_buffer, name])
+#         print(name)
+#         for child in node.childNodes:
+#             parse_paths_from_file(child, name)   
+
+# def parse_paths(path):
+#     for file in list(map(lambda f: join(path, f), next(walk(path), (None, None, []))[2])):
+#         name = file.split('/')[-1].replace('xml', 'txt')
+#         print(f"Look path from {file}")
+#         parse_paths_from_file(minidom.parse(file).childNodes[0])  
+#         break       
 
 if __name__ == "__main__":
-    # parse_plays(dracor_folder)
-    # generate_graph(dracor_folder)
-    # export_svg(outputs_folder)
-    nodes, links = find_same_nodes(dracor_folder)
-    create_common_tree(nodes, links)
-    # parse_paths(dracor_folder)
+
+    parser = argparse.ArgumentParser(description='Consolide ton réseau.')  
+    parser.add_argument(
+        '-a', '--acts',
+        help="Selects only the files respecting a number minimum and maximum of an act.",
+        metavar='N', type=int,
+        nargs=2)
+    parser.add_argument(
+        '-c', '--clean',
+        help="Cleans up given folders",
+        nargs='?')
+    parser.add_argument(
+        '-d', '--directory',
+        help="Selects a folder to run the program (by default, Dracor).",
+        nargs='?')
+    parser.add_argument(
+        '-i', '--intersection',
+        help="Generates the intersection of the structure of each XML file as a dot file.",
+        action="store_true")
+    parser.add_argument(
+        '-p', '--precision',
+        help="Explains act, scene, and line numbers, as well as characters.",
+        action="store_true")
+    parser.add_argument(
+        '-t', '--tree',
+        help="Generates for each selected XML file its structure in a dot file, in the form of a tree.",
+        type=int, default=False,
+        nargs='?')
+    parser.add_argument(
+        '-v', '--verbose', 
+        help="Generates for each selected XML file its structure in a text file", 
+        action="store_true")
+    
+    args = parser.parse_args()
+    print(args)
+
+    clean = args.clean
+    if clean:
+        clean_outputs_directory(abspath(join(root, abspath(join(root, clean)).replace('Corpus', 'trees').replace('corpus', 'trees'))))
+
+    directory = args.directory
+    if directory:
+        inputs_folder = abspath(join(root, directory))
+        outputs_folder = abspath(join(root, directory.replace('Corpus', 'trees').replace('corpus', 'trees')))
+    
+    if not exists(outputs_folder):
+        os.makedirs(outputs_folder)
+
+    if args.verbose:
+        parse_plays(inputs_folder)
+    
+    if args.tree is not False:
+        if args.tree is not None:
+            if args.tree < 0:
+                raise ValueError("argument -t/--tree : int height value must be positive.")
+            generate_graph(inputs_folder, args.tree)
+        else:
+            generate_graph(inputs_folder)
+    
+    if args.intersection:
+        nodes, links = find_same_nodes(inputs_folder)
+        common_file = join(common_trees_folder, get_file_name(inputs_folder).replace('corpus', 'common').replace('Corpus', 'common'))
+        create_common_tree(nodes, links, common_file)
+
+
+    
+
 
